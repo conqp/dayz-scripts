97c9d70ceebc397aca4568990d2c341e1c73887c
Richard Neumann <mail@richard-neumann.de>
2022-02-03 22:50:53 +0100

    Rewrite custom weapon creation

--- src/spawnInventory.c
+++ src/spawnInventory.c
@@ -28,20 +28,26 @@ const autoptr array<string> M4A1_ATTACHMENTS = {
         "M4_OEBttstck",
         "M4_RISHndgrd",
         "UniversalLight",
+        "M4_Suppressor",
         "M4_T3NRDSOptic",
         "Battery9V",
-        "Battery9V",
-        "M4_Suppressor",
-        "Mag_STANAGCoupled_30Rnd"
+        "Battery9V"
 };
 const autoptr array<string> GLOCK19_ATTACHMENTS = {
         "PistolSuppressor",
-        "Mag_Glock_15Rnd"
+        "FNP45_MRDSOptic",
+        "TLRLight",
+        "Battery9V",
+        "Battery9V"
 };
 
 void addSpawnInventory(GameInventory inventory)
 {
         addInventoryItems(inventory, SPAWN_LOOT);
-        addAttachments(inventory.CreateInInventory("M4A1"), M4A1_ATTACHMENTS);
-        addAttachments(inventory.CreateInInventory("Glock19"), GLOCK19_ATTACHMENTS);
+        auto m4a1 = M4A1_Base.CreateWeaponWithAmmo("M4A1", "Mag_STANAGCoupled_30Rnd");
+        addAttachments(m4a1, M4A1_ATTACHMENTS);
+        inventory.AddEntityToInventory(m4a1);
+        auto glock19 = Glock19_Base.CreateWeaponWithAmmo("Glock19", "Mag_Glock_15Rnd");
+        addAttachments(glock19, GLOCK19_ATTACHMENTS);
+        inventory.AddEntityToInventory(glock19);
 }
